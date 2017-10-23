1a11dcafb09044e3f5bc0fc5fbe47e0b7df4fd34
dell <Brad.Xie@szyuxuan.com>
2017-10-23 10:58:14 +0800

    10.23

--- app/src/main/java/com/xie/brad/myretrofit/constant/ServerUrl.java
+++ app/src/main/java/com/xie/brad/myretrofit/constant/ServerUrl.java
@@ -1,7 +1,7 @@
 package com.xie.brad.myretrofit.constant;
 
 /**
- * Created by wangjian on 2016/9/20.
+ * 这里是请求的链接
  */
 public class ServerUrl {
      public static final String ServerUrl = "http://test.whgxwl.com:8000/FS_beta/";
